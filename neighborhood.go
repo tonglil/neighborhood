ffaf7df2b640080a306c28415888982f0fa86735
Tony Li <tonyli.lives@gmail.com>
2016-03-18 12:03:12 -0700

    add summary stats

--- neighborhood.go
+++ neighborhood.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -18,6 +19,9 @@ func check(e error) {
 }
 
 func main() {
+	topPtr := flag.Int("top", 0, "The number of nodes to count an average rtt for [0 = all]")
+	flag.Parse()
+
 	fmt.Println("Determining what the neighborhood looks like!")
 	fmt.Println("Finding the rtts from Consul...\n")
 
@@ -52,12 +56,45 @@ func main() {
 	err = cmd.Wait()
 	check(err)
 
-	rtt(nodes)
+	ranked := rtt(nodes)
+
+	for rank, pair := range ranked {
+		fmt.Printf("%v: %v - %vms\n", rank, pair.Key, pair.Value)
+	}
+
+	avg, min, max := stats(ranked, *topPtr)
+
+	fmt.Printf("For the top %v nodes...\n", *topPtr)
+	fmt.Printf("Average: %vms, Min: %vms, Max: %vms\n", avg, min, max)
 
 	return
 }
 
-func rtt(nodes map[string]string) {
+func stats(nodes PairList, count int) (float64, float64, float64) {
+	var (
+		avg, min, max, sum float64
+	)
+
+	if count == 0 {
+		count = len(nodes)
+	}
+
+	selected := nodes[:count]
+
+	min = selected[0].Value
+	max = selected[count].Value
+	sum = 0
+
+	for _, pair := range selected {
+		sum += pair.Value
+	}
+
+	avg = sum / float64(count)
+
+	return avg, min, max
+}
+
+func rtt(nodes map[string]string) PairList {
 	times := make(map[string]float64)
 
 	r, _ := regexp.Compile("\\d+.\\d+")
@@ -82,11 +119,7 @@ func rtt(nodes map[string]string) {
 		times[node] = f
 	}
 
-	ranked := ranker(times)
-
-	for rank, pair := range ranked {
-		fmt.Printf("%v: %v - %vms\n", rank, pair.Key, pair.Value)
-	}
+	return ranker(times)
 }
 
 func printCommand(cmd *exec.Cmd) {
